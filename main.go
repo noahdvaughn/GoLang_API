62de418b62cfaa561c8865e767b64225482a9a6e
Noah Vaughn <noahdvaughn@gmail.com>
2024-01-19 21:39:05 -0500

    POST route created

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"net/http"
-
+	// "errors"
 	"github.com/gin-gonic/gin"
 )
 type book struct{
 	ID string `json:"id"`
 	Title   string `json:"title"`
 	Author   string `json:"author"`
-	Quantity   int `json:"Quantity"`
+	Quantity   int `json:"quantity"`
 }
 
 var books = []book{
@@ -20,9 +20,23 @@ var books = []book{
 func getBooks(c *gin.Context){
 	c.IndentedJSON(http.StatusOK, books)
 }
+func createBook(c *gin.Context){
+	var newBook book 
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+
+	c.IndentedJSON(http.StatusCreated, newBook)
+
+}
 
 func main(){
 	router := gin.Default()
+
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 }
