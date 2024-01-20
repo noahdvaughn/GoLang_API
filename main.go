22bf2a9371b1e980cfca6593685e95d22ad53044
Noah Vaughn <noahdvaughn@gmail.com>
2024-01-19 22:20:26 -0500

    FIND function working

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"errors"
 	"net/http"
-	// "errors"
+
 	"github.com/gin-gonic/gin"
 )
 type book struct{
@@ -22,21 +23,37 @@ func getBooks(c *gin.Context){
 }
 func createBook(c *gin.Context){
 	var newBook book 
-
 	if err := c.BindJSON(&newBook); err != nil {
 		return
 	}
-
 	books = append(books, newBook)
-
 	c.IndentedJSON(http.StatusCreated, newBook)
+}
 
+func getBookById(id string) (*book, error){
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	} 
+	return nil, errors.New("book not found")
+}
+
+func bookById(c *gin.Context){
+	id := c.Param("id")
+	book, err := getBookById(id)
+
+	if err != nil {
+		return
+	}
+	c.IndentedJSON(http.StatusOK, book)
 }
 
 func main(){
 	router := gin.Default()
 
 	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookById)
 	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 }
