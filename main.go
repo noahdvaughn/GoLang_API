a7975e1227480d3040310dfdc3d8c00db4458a81
Noah Vaughn <noahdvaughn@gmail.com>
2024-01-20 21:41:53 -0500

    created PATCH route

--- main.go
+++ main.go
@@ -6,23 +6,25 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
-type book struct{
-	ID string `json:"id"`
-	Title   string `json:"title"`
+
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
 	Author   string `json:"author"`
-	Quantity   int `json:"quantity"`
+	Quantity int    `json:"quantity"`
 }
 
 var books = []book{
 	{ID: "1", Title: "In search of lost time", Author: "Marcel Proust", Quantity: 2},
-	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5}, 
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
-func getBooks(c *gin.Context){
+
+func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
-func createBook(c *gin.Context){
-	var newBook book 
+func createBook(c *gin.Context) {
+	var newBook book
 	if err := c.BindJSON(&newBook); err != nil {
 		return
 	}
@@ -30,16 +32,16 @@ func createBook(c *gin.Context){
 	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
-func getBookById(id string) (*book, error){
+func getBookById(id string) (*book, error) {
 	for i, b := range books {
 		if b.ID == id {
 			return &books[i], nil
 		}
-	} 
+	}
 	return nil, errors.New("book not found")
 }
 
-func bookById(c *gin.Context){
+func bookById(c *gin.Context) {
 	id := c.Param("id")
 	book, err := getBookById(id)
 
@@ -50,11 +52,33 @@ func bookById(c *gin.Context){
 	c.IndentedJSON(http.StatusOK, book)
 }
 
-func main(){
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if ok == false {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid query parameter"})
+		return
+	}
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available"})
+		return
+	}
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func main() {
 	router := gin.Default()
 
 	router.GET("/books", getBooks)
 	router.GET("/books/:id", bookById)
 	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
 	router.Run("localhost:8080")
 }
